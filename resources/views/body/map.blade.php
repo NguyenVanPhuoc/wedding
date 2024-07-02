a3b1a28150b3f2cab204fb80e5ec0c739d0840a4
NguyenVanPhuoc <vanphuoc260797@gmail.com>
2024-07-02 10:40:00 +0700

    Update google map

--- resources/views/body/map.blade.php
+++ resources/views/body/map.blade.php
@@ -1,14 +1,12 @@
 <section id="map">
-    <div class="container-fluid">
-        <div class="row">
-            <div class="col-md-6">
-                <h3 class="name-map item-map">TIỆC CƯỚI NHÀ NAM</h3>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3825.071709938538!2d107.48754!3d16.522477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTbCsDMxJzIwLjkiTiAxMDfCsDI5JzE1LjEiRQ!5e0!3m2!1svi!2s!4v1719886764678!5m2!1svi!2s" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
-            <div class="col-md-6 item-map map-girl">
-                <h3 class="name-map">TIỆC CƯỚI NHÀ NỮ</h3>
-                <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3834.24800867985!2d108.112664!3d16.052615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTbCsDAzJzA5LjQiTiAxMDjCsDA2JzQ1LjYiRQ!5e0!3m2!1svi!2s!4v1719886879496!5m2!1svi!2s" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
-            </div>
+    <div class="row">
+        <div class="col-md-6">
+            <h3 class="name-map item-map">TIỆC CƯỚI NHÀ NAM</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3825.071709938538!2d107.48754!3d16.522477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTbCsDMxJzIwLjkiTiAxMDfCsDI5JzE1LjEiRQ!5e0!3m2!1svi!2s!4v1719886764678!5m2!1svi!2s" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
+        <div class="col-md-6 item-map map-girl">
+            <h3 class="name-map">TIỆC CƯỚI NHÀ NỮ</h3>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3834.24800867985!2d108.112664!3d16.052615!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMTbCsDAzJzA5LjQiTiAxMDjCsDA2JzQ1LjYiRQ!5e0!3m2!1svi!2s!4v1719886879496!5m2!1svi!2s" width="100%" height="500" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </div>
     </div>
 </section>
